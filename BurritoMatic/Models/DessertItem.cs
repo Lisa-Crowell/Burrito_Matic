3f23c1e0d24bfe53f7708630fb9ba074eb11631b
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:48:22 -0500

    add dessert item class

---
+++ BurritoMatic/Models/DessertItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents a drink item on the Burrito Matic menu. 
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class DessertItem : OrderItemBase
+{
+    
+}
