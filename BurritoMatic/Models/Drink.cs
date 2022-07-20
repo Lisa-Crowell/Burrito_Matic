bf5fadf9b3ec75dd28af40ef40443c39b2d88b4e
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:22:21 -0500

    add drink class

---
+++ BurritoMatic/Models/Drink.cs
@@ -0,0 +1,21 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents drink options for BurritoMatic.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Drink : MenuItemBase
+{
+    /// <summary>
+    /// This is the enum for the size of the drink in US ounces.
+    /// </summary>
+    public enum Size
+    {
+        Small = 16,
+        Medium = 20,
+        Large = 24
+    }
+    public Size DrinkSize { get; set; }
+    public bool IsAlcoholic { get; set; }
+    public decimal Price { get; set; }
+}
