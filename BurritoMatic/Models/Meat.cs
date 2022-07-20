5ee7fe0cc83378d1b00f5d26a11f594281c96e69
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:20:49 -0500

    add meat class

---
+++ BurritoMatic/Models/Meat.cs
@@ -0,0 +1,10 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents the Burrito Matic's meat options.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Meat : IngredientBase
+{
+
+}
