f19ad8b94bbe26b564f6ee23767608f0c1141150
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:43:29 -0500

    add category class

---
+++ BurritoMatic/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This is the model for the BurritoMatic.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Category : MenuItemBase
+{
+
+}
