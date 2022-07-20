efcbcc9a4c484bdb2bb50b9971caa0fb9684f893
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 10:15:38 -0500

    add order service class

---
+++ BurritoMatic/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using BurritoMatic.Models;
+
+namespace BurritoMatic.Services;
+
+/// <summary>
+/// The implementation of the Burrito Matic IOrderService.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class OrderService : IOrderService
+{
+    public Order CreateOrder(List<OrderItemBase> orderItems, PaymentRecord paymentRecord)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Order CancelOrder(Order order)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Order CompleteOrder(Order order)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TransmitOrder(Order order)
+    {
+        throw new System.NotImplementedException();
+    }
+}
