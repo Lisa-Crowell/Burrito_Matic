e0a391b84090eaabb52b849ce93d6bf1a45ded7a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:04:54 -0500

    add ingredient base

---
+++ BurritoMatic/Models/IngredientBase.cs
@@ -0,0 +1,15 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This is the abstract base class for Burrito Matic's menu items.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public abstract class IngredientBase
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Surcharge { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+}
