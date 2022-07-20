d2cda4eb4d13999999769416efa4a48d7a13c656
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:39:19 -0500

    add order item class

---
+++ BurritoMatic/Models/OrderItemBase.cs
@@ -0,0 +1,11 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This is the abstract base class for Burrito Matic's order items.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public abstract class OrderItemBase
+{
+    public MenuItemBase MenuItem { get; set; }
+    public int Quantity { get; set; }
+}
