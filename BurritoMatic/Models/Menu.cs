e4ce973f11b067af422d1a111d24bec622c2ac0c
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:42:13 -0500

    add menu class

---
+++ BurritoMatic/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents the menu items on the Burrito Matic menu.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Menu
+{
+    public List<MenuItemBase> MenuItems { get; set; }
+}
