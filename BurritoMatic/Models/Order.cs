612909fc357e5f931ab900289b18892017f0a29b
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:41:38 -0500

    add order class

---
+++ BurritoMatic/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents an order for Burrito Matic.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Order
+{
+    public List<OrderItemBase> Items { get; set; }
+    public int OrderId { get; set; }
+    public PaymentRecord PaymentRecord { get; set; }
+    public bool IsComplete { get; set; }
+    public bool IsCancelled { get; set; }
+}
