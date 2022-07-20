fb9785aef3403e9e700a7b6bde432f9c0cda062f
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:24:38 -0500

    add salsa class

---
+++ BurritoMatic/Models/Salsa.cs
@@ -0,0 +1,10 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents the BurritoMatic's salsa options.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Salsa : IngredientBase
+{
+
+}
