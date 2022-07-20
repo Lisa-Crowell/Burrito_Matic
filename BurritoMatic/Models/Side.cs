3d6c640f3dbacf6c50c9d9ca0d4f9035958d8aa5
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:26:52 -0500

    add side class

---
+++ BurritoMatic/Models/Side.cs
@@ -0,0 +1,10 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents Burrito Matic's side options.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Side : MenuItemBase
+{
+    public decimal Price { get; set; }
+}
