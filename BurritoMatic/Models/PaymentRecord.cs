b61d8eb5b97c56db90de1c3b21cfc0e8038caff7
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:37:21 -0500

    add payment record class

---
+++ BurritoMatic/Models/PaymentRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents a payment record for Burrito Matic for an order.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class PaymentRecord
+{
+    public int PaymentRecordId { get; set; }
+    public decimal Subtotal { get; set; }
+    public decimal Tax { get; set; }
+    public decimal Total { get; set; }
+    public bool IsPaid { get; set; }
+    public DateTime DatePaid { get; set; }
+    public string ConfirmationNumber { get; set; }
+}
