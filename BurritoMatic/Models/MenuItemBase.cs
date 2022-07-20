d18f0d643b0efeb6a4dfdb0c821a291c4b88f615
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:17:13 -0500

    add menu item base class

---
+++ BurritoMatic/Models/MenuItemBase.cs
@@ -0,0 +1,13 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This abstract class is the base class for Burrito Matic's menu items.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public abstract class MenuItemBase
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
