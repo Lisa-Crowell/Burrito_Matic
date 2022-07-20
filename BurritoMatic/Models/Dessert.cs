b97fb062a2b9c3347fbd38a71b18d61b59e6b61f
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:29:33 -0500

    add dessert class

---
+++ BurritoMatic/Models/Dessert.cs
@@ -0,0 +1,10 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents BurritoMatic's dessert options.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Dessert : MenuItemBase
+{
+    public decimal Price { get; set; }
+}
