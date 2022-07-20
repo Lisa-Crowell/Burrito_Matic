16697ff1ee3966507652e9e983647abab0061cd1
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:46:00 -0500

    add burrito item class

---
+++ BurritoMatic/Models/BurritoItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents a burrito item on the BurritoMatic menu.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class BurritoItem : OrderItemBase
+{
+    public List<Meat> Meats { get; set; }
+    public List<Topping> Toppings { get; set; }
+    public List<Salsa> Salsas { get; set; }
+    public List<Foundation> Foundations { get; set; }
+}
