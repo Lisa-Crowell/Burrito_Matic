4891d74e05573c9bbd042b53cb0b4a1afbbea2f9
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-20 09:32:05 -0500

    add burrito class

---
+++ BurritoMatic/Models/Burrito.cs
@@ -0,0 +1,13 @@
+﻿namespace BurritoMatic.Models;
+
+/// <summary>
+/// This class represents a burrito.
+/// </summary>
+/// <author>lisa.l.crowell@gmail.com</author>
+public class Burrito : MenuItemBase
+{
+    public int MaxMeat { get; set; }
+    public int MaxToppings { get; set; }
+    public int MaxSalsas { get; set; }
+    public decimal Price { get; set; }
+}
